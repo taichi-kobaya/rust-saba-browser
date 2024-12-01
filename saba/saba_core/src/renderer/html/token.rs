11e5061675f73a3f839b8c92fb96b5093b3b220f
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2024-12-01 20:13:12 +0900

    HTMLの字句解析機を実装開始
    - HtmlTokenizer構造体を追加し初期化処理を実装
    - HtmlToken列挙型を追加しHTMLトークンの種類を定義
    - Attribute構造体を追加し属性名と値の処理を実装
    - ステートマシンを定義しHTML字句解析で使用する状態を列挙
    
    テストは未実施。トークナイザーの実装が完了後に対応予定。

---
+++ saba/saba_core/src/renderer/html/token.rs
@@ -0,0 +1,84 @@
+use crate::renderer::html::attribute::Attribute;
+use alloc::string::String;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum HtmlToken {
+    // 開始タグ
+    StartTag {
+        tag: String,
+        self_closing: bool,
+        attributes: Vec<Attribute>,
+    },
+    // 終了タグ
+    EndTag {
+        tag: String,
+    },
+    // 文字
+    Char(char),
+    // ファイルの終了(End of File)
+    Eof,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum State {
+    /// https://html.spec.whatwg.org/multipage/parsing.html#data-state
+    Data,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#tag-open-state
+    TagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#end-tag-open-state
+    EndTagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#tag-name-state
+    TagName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-name-state
+    BeforeAttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-name-state
+    AttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-name-state
+    AfterAttributeName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#before-attribute-value-state
+    BeforeAttributeValue,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(double-quoted)-state
+    AttributeValueDoubleQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(single-quoted)-state
+    AttributeValueSingleQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#attribute-value-(unquoted)-state
+    AttributeValueUnquoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#after-attribute-value-(quoted)-state
+    AfterAttributeValueQuoted,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#self-closing-start-tag-state
+    SelfClosingStartTag,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-state
+    ScriptData,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-less-than-sign-state
+    ScriptDataLessThanSign,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-open-state
+    ScriptDataEndTagOpen,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#script-data-end-tag-name-state
+    ScriptDataEndTagName,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#temporary-buffer
+    TemporaryBuffer,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HtmlTokenizer {
+    state: State,
+    pos: usize,
+    reconsume: bool,
+    latest_token: Option<HtmlToken>,
+    input: Vec<char>,
+    buf: String,
+}
+
+impl HtmlTokenizer {
+    pub fn new(html: String) -> Self {
+        Self { 
+            state: State::Data, 
+            pos: 0, 
+            reconsume: false, 
+            latest_token: None, 
+            input: html.chars().collect(), 
+            buf: String::new(),
+        }
+    }
+}
