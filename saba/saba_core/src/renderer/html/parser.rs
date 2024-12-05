8f619358daa48ac83ef2d814672934a3eae8423c
Taichi Kobayashi <higoronookonaigadaiji@gmail.com>
2024-12-05 17:11:58 +0900

    HTMLの構文解析機能を実装開始
    - ツリーの構築のため、各構造体を定義
        - Node / Window / Element / Parser

---
+++ saba/saba_core/src/renderer/html/parser.rs
@@ -0,0 +1,29 @@
+use crate::renderer::dom::node::{Node, Window};
+use crate::renderer::dom::node::Window;
+use crate::renderer::html::token::HtmlTokenizer;
+use alloc::rc::Rc;
+use alloc::vec::Vec;
+use core::cell::RefCell;
+
+#[derive(Debug, Clone)]
+pub struct HtmlParser {
+    window: Rc<RefCell<Window>>,
+    mode: InsertionMode,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#original-insertion-mode
+    original_insertion_mode: InsertionMode,
+    /// https://html.spec.whatwg.org/multipage/parsing.html#the-stack-of-open-elements
+    stack_of_open_elements: Vec<Rc<RefCell<Node>>>,
+    t: HtmlTokenizer,
+}
+
+impl HtmlParser {
+    pub fn new(t: HtmlTokenizer) -> Self {
+        Self {
+            window: Rc::new(RefCell::new(Window::new())),
+            mode:   InsertionMode::Initial,
+            original_insertion_mode: InsertionMode::Initial,
+            stack_of_open_elements: Vec::new(),
+            t,
+        }
+    }    
+}
